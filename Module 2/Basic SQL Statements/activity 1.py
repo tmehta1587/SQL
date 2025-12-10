936a9bb849ad8a78c176756afe55348787b53c09
tmehta1587 <tmehta1587@gmail.com>
2025-12-10 11:38:09 +0700

    commit

--- Module 2/Basic SQL Statements/activity 1.py
+++ Module 2/Basic SQL Statements/activity 1.py
@@ -7,29 +7,34 @@ database = 'database.sqlite'
 conn = sqlite3.connect(database)
 
 conn.execute("""
-CREATE TABLE IF NOT EXISTS Match(
-    Match_Id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS Match1(
+    Match_Id INTEGER PRIMARY KEY,
     Season_Id INTEGER,
     Match_Winner TEXT,
-    Win_Margin REAL,
+    Win_Margin INTEGER,
     Man_of_the_Match TEXT
-    
 );
 """)
 
 print("Table 'Match' checked/created successfully ")
 
 sample_data = [
-    (9, 'Team A', 45, 'Player 1', 101),
-    (9, 'Team B', 22, 'Player 2', 102),
-    (9, 'Team A', 10, 'Player 3', 101),
-    (8, 'Team C', 60, 'Player 1', 103),
+    (1,9, 'Team A', 45, 'Player 1'),
+    (2,9, 'Team B', 22, 'Player 2'),
+    (3,9, 'Team A', 10, 'Player 3'),
+    (4,8, 'Team C', 60, 'Player 1')
 ]
 
 conn.executemany("""
-INSERT INTO Match( Season_Id, Match_Winner, Win_Margin, Man_of_the_Match) VALUES (?,?,?,?,?);
+
+INSERT INTO Match1 (Match_Id, Season_Id, Match_Winner, Win_Margin, Man_of_the_Match)
+
+VALUES (?, ?, ?, ?, ?);
+
 """, sample_data)
+
 conn.commit()
+
 print("Sample data inserted")
 
 tables = pd.read_sql("""
@@ -40,27 +45,20 @@ WHERE type='table';
 print("\n Tables in Database:")
 print(tables)
 
-matches = pd.read_sql("SELECT * FROM Match;", conn)
+matches = pd.read_sql("SELECT * FROM Match1;", conn)
 print("\n Match Table Preview:")
 print(matches.head())
 
 result1 = pd.read_sql("""
-SELECT Match_Winner, AVG(Win_Margin) AS Avg_Margin
-FROM match
+SELECT Match_Winner, AVG(Win_Margin) AS Avg_Win_Margin
+FROM Match1
 WHERE Season_Id = 9
 GROUP BY Match_Winner
-ORDER BY Avg_Min_Margin;
+ORDER BY Avg_Win_Margin;
 """, conn)
 print("\nAverage Win Margin(Season 9):")
 print(result1)
 
-result2 = pd.read_sql("""
-SELECT COUNT(DISTINCT Venue_Id) AS Venue_Count
-FROM match
-WHERE Season_Id = 9;
-""", conn)
-print("\nVEnue Count(Season 9):")
-print(result2)
 
 result3 = pd.read_sql("""
 SELECT 
@@ -68,14 +66,14 @@ SELECT
     MAX(Win_Margin) AS Max_Win_Margin, 
     AVG(Win_Margin) AS Avg_Min_Margin,
     COUNT(DISTINCT Man_of_the_MAtch) AS Unique_MOM_Count
-FROM Match;
+FROM Match1;
 """, conn)
 print("\n Stats Across All Seasons: ")
 print(result3)
 
 result4 = pd.read_sql("""
 SELECT SUM(Win_Margin) AS Total_Win_Margin
-FROM match
+FROM Match1
 WHERE Season_Id = 9;
 """, conn)
 print("\nTotal Win Margin (season 9):")
